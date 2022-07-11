a441fa86a4662f81cd65cc1a72a30e97212d8acb
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <jankes6463@gmail.com>
2022-07-11 13:11:20 +0200

    Created project

---
+++ GuestBook/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GuestBook.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Content { get; set; }
+
+        public Post()
+        {
+            Id = Guid.NewGuid();
+            CreateTime = DateTime.Now;
+        }
+        public Post(string name, string email, string content)
+        {
+            Id= Guid.NewGuid();
+            CreateTime= DateTime.Now;
+            Name= name;
+            Email= email;
+            Content= content;
+        }
+    }
+}
